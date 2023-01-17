8c92d90df377773f131212dd407e20f2a0c1196d
hemavinti181 <109754083+hemavinti181@users.noreply.github.com>
2023-01-17 16:34:39 +0530

    first commit

---
+++ smbt/smbt/smbtproject/smbtapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from smbtapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index_page')
+]
